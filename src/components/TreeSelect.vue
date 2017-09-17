498be3b080b33a723ee8f73124c2ba1093f3b79d
APZelos <a.p.zelos@gmail.com>
2017-09-17 12:44:57 +0300

    Specified the style lang to use scss.

--- src/components/TreeSelect.vue
+++ src/components/TreeSelect.vue
@@ -117,7 +117,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 .treeselect {
+  &__select {}
 }
 </style>
