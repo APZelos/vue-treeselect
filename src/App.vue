7448d80663d7ff424659aed0de9ed43986d4945b
APZelos <a.p.zelos@gmail.com>
2017-09-23 20:15:44 +0300

    Created array of options for test.

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,32 @@ export default {
   data () {
     return {
       value: '2, 5',
-      options: [],
+      options: [
+        {
+          id: 1,
+          label: '1',
+          children: [
+            {
+              id: 2,
+              label: '2'
+            },
+            {
+              id: 3,
+              label: '3',
+              children: [
+                {
+                  id: 4,
+                  label: '4'
+                },
+                {
+                  id: 5,
+                  label: '5'
+                }
+              ]
+            }
+          ]
+        }
+      ],
       searchable: true
     }
   }
