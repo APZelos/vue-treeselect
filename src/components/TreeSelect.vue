e18e8049a188319f681392243f95dd0a3b8927c7
APZelos <a.p.zelos@gmail.com>
2017-09-25 12:40:46 +0300

    Defined optionList computed property,

--- src/components/TreeSelect.vue
+++ src/components/TreeSelect.vue
@@ -180,6 +180,85 @@ export default {
      */
     isAnyOptionSelected () {
       return this.selectedOptions && this.selectedOptions.length > 0
+    },
+    /**
+     * A list created by the original list of options given to component as a property
+     * and configured with a series of additional properties.
+     */
+    optionList () {
+      /**
+       * Creates a configured option and defines a series of properties
+       * that are required for the component to handle it.
+       *
+       * @param {Object} option The original option.
+       * @param {Boolean} [isParentSelected=false] A flag that indicates if the parent that contains the given option is marked as selected.
+       * @returns A configured option.
+       */
+      const configureOption = (option, isParentSelected = false) => {
+        // Creates a copy of the option
+        // that is going to be configured.
+        const optionObj = {}
+        optionObj[this.idProp] = option[this.idProp]
+        optionObj[this.labelProp] = option[this.labelProp]
+        optionObj[this.childrenProp] = []
+        optionObj.hasChildSelected = false
+        optionObj.hasChildSearchResult = false
+        // An option is considered selected when its id value is included in the values array
+        // or its parent is marked as selected.
+        // (because the selection of an option that has children indicates the selection of all of its children)
+        optionObj.isSelected = isParentSelected || Array.prototype.includes.call(this.values, optionObj[this.idProp])
+        // An option is considered a candidate for a search result if its label value contains the given search query.
+        optionObj.isSearchResult = this.isSearching && !!~String.prototype.indexOf.call(optionObj[this.labelProp], this.searchQuery)
+        // If the option doesn't contain any children
+        // no further configuration needs to be made.
+        const childrenCount = Object.prototype.hasOwnProperty.call(option, this.childrenProp) && option[this.childrenProp].length
+        if (!childrenCount) return optionObj
+        const { selectedChildrenCount, isAnyChildrenSearchResult } = configureChildren(option[this.childrenProp], optionObj[this.childrenProp], optionObj.isSelected)
+        optionObj.hasChildSearchResult = this.isSearching && isAnyChildrenSearchResult
+        // An option can be marked that has selected children:
+        //  * if at least one of its children is selected
+        //  * if at least one of its children contains selected children
+        // but not if all of them are selected.
+        optionObj.hasChildSelected = selectedChildrenCount && (selectedChildrenCount < childrenCount)
+        // If the option isn't already marked as selected but all of its children are selected
+        // then the option must also be marked selected.
+        optionObj.isSelected = optionObj.isSelected || (selectedChildrenCount && selectedChildrenCount === childrenCount)
+        return optionObj
+      }
+
+      /**
+       * Fills an array with configured children and returns a series of values to help the configuration of the parent.
+       *
+       * @param {Array} children The children of the original option.
+       * @param {Array} childrenList The list of the configured option that is going to hold the configured children.
+       * @param {Boolean} [isParentSelected=false] A flag that indicates if the option that contains the given children is marked as selected.
+       * @returns The count of selected children and if any children is considered a candidate for a search result.
+       */
+      const configureChildren = (children, childrenList, isParentSelected = false) => {
+        let selectedChildrenCount = 0
+        let isAnyChildrenSearchResult = false
+        for (let i = 0; i < children.length; i++) {
+          const child = configureOption(children[i], isParentSelected)
+          // If the configured child is marked as selected
+          // then the count of selected children must be increased by 1
+          if (child.isSelected) selectedChildrenCount++
+          // If the configured child contains children that are marked as selected
+          // then the selected children must be increased by 0.5,
+          // so the total count will always be smaller than the length of the children.
+          else if (child.hasChildSelected) selectedChildrenCount += 0.5
+          // If the child, or any of its children, is considered a candidate for a search result
+          // then the returned flag must be true.
+          isAnyChildrenSearchResult = isAnyChildrenSearchResult || child.isSearchResult || child.hasChildSearchResult
+          childrenList.push(child)
+        }
+        return { selectedChildrenCount, isAnyChildrenSearchResult }
+      }
+
+      const list = []
+      for (let i = 0; i < this.options.length; i++) {
+        list.push(configureOption(this.options[i]))
+      }
+      return list
     }
   },
   methods: {
