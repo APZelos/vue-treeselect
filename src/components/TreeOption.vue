c370f517ffdfa7de2d7818991dd62cb3ad1f4847
APZelos <a.p.zelos@gmail.com>
2017-09-25 12:50:30 +0300

    Created empty TreeOption component.

---
+++ src/components/TreeOption.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="treeoption">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TreeOption',
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.treeoption {
+  width: 100%;
+}
+</style>
