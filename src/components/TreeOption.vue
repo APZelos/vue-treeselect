c233d21e96f80eaec57fee93f5b9c1624f6abb66
APZelos <a.p.zelos@gmail.com>
2017-09-26 13:20:52 +0300

    Defined label computed property.

--- src/components/TreeOption.vue
+++ src/components/TreeOption.vue
@@ -54,6 +54,12 @@ export default {
      */
     id () {
       return this.option[this.idProp]
+    },
+    /**
+     * The option's label value.
+     */
+    label () {
+      return this.option[this.labelProp]
     }
   },
   methods: {}
