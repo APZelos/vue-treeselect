683dd61c6707ffd4e5884817529c7f12b8258aba
APZelos <a.p.zelos@gmail.com>
2017-10-06 12:17:43 +0300

    Add comment for getId method

--- src/mixins/idProp.js
+++ src/mixins/idProp.js
@@ -12,6 +12,12 @@ export default {
     }
   },
   methods: {
+    /**
+     * Gets an option and returns its id value.
+     *
+     * @param {any} option The option which id we want to get.
+     * @returns The value of the property with the name of idProp.
+     */
     getId (option) {
       return option[this.idProp]
     }
